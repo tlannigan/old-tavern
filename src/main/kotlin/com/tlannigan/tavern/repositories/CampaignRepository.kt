c6fbc0411caa69e8eaf33093ab2a7b3d61a68e7f
tlannigan <tristanlannigan@gmail.com>
2022-06-21 17:53:46 -0600

    Add collection repositories

---
+++ src/main/kotlin/com/tlannigan/tavern/repositories/CampaignRepository.kt
@@ -0,0 +1,24 @@
+package com.tlannigan.tavern.repositories
+
+import com.mongodb.client.MongoDatabase
+import com.tlannigan.tavern.utils.DatabaseManager
+
+class CampaignRepository(private val db: MongoDatabase = DatabaseManager.db) {
+
+    fun create() {
+
+    }
+
+    fun find() {
+
+    }
+
+    fun update() {
+
+    }
+
+    fun delete() {
+
+    }
+
+}
