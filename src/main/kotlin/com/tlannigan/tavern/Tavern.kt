19c3235f486832ec6e7a36a977cd8c44824ec65b
tlannigan <tristanlannigan@gmail.com>
2022-06-21 17:03:40 -0600

    Add config and database init/deinit

--- src/main/kotlin/com/tlannigan/tavern/Tavern.kt
+++ src/main/kotlin/com/tlannigan/tavern/Tavern.kt
@@ -1,15 +1,21 @@
 package com.tlannigan.tavern
 
+import com.tlannigan.tavern.utils.ConfigManager
+import com.tlannigan.tavern.utils.DatabaseManager
 import org.bukkit.plugin.java.JavaPlugin
 
 class Tavern : JavaPlugin() {
 
-    override fun onEnable() {
+    private val config = ConfigManager(this)
+    private val db = DatabaseManager(this)
 
+    override fun onEnable() {
+        config.initialize()
+        db.initialize()
     }
 
     override fun onDisable() {
-
+        DatabaseManager.mongoClient.close()
     }
 
 }
