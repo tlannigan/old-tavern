c4e01b053f21ec58ec080f4c9535ae73baa9acd8
tlannigan <tristanlannigan@gmail.com>
2022-06-22 16:01:34 -0600

    Implement campaign repository CRUD methods

--- src/main/kotlin/com/tlannigan/tavern/repositories/CampaignRepository.kt
+++ src/main/kotlin/com/tlannigan/tavern/repositories/CampaignRepository.kt
@@ -1,24 +1,35 @@
 package com.tlannigan.tavern.repositories
 
 import com.mongodb.client.MongoDatabase
+import com.mongodb.client.result.DeleteResult
+import com.mongodb.client.result.InsertOneResult
+import com.mongodb.client.result.UpdateResult
+import com.tlannigan.tavern.models.TCampaign
 import com.tlannigan.tavern.utils.DatabaseManager
+import org.litote.kmongo.*
 
-class CampaignRepository(private val db: MongoDatabase = DatabaseManager.db) {
+class CampaignRepository(db: MongoDatabase = DatabaseManager.db) {
 
-    fun create() {
+    private val campaigns = db.getCollection<TCampaign>("campaigns")
 
+    fun create(campaign: TCampaign): InsertOneResult {
+        return campaigns.insertOne(campaign)
     }
 
-    fun find() {
-
+    fun find(campaignId: Id<TCampaign>): TCampaign? {
+        return campaigns.findOne(TCampaign::id eq campaignId)
     }
 
-    fun update() {
-
+    fun findMany(): MutableList<TCampaign> {
+        return campaigns.find(TCampaign::playerLimit eq 8).toMutableList()
     }
 
-    fun delete() {
+    fun update(campaign: TCampaign): UpdateResult {
+        return campaigns.updateOne(TCampaign::id eq campaign.id)
+    }
 
+    fun delete(campaign: TCampaign): DeleteResult {
+        return campaigns.deleteOne(TCampaign::id eq campaign.id)
     }
 
 }
