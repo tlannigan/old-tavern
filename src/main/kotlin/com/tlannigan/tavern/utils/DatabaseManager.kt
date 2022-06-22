5a04c2cfab63c3d7f05119860bde4b7508c1de72
tlannigan <tristanlannigan@gmail.com>
2022-06-22 16:03:46 -0600

    Add specified UUID representation in MongoClient settings

--- src/main/kotlin/com/tlannigan/tavern/utils/DatabaseManager.kt
+++ src/main/kotlin/com/tlannigan/tavern/utils/DatabaseManager.kt
@@ -1,10 +1,12 @@
 package com.tlannigan.tavern.utils
 
 import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoDatabase
+import org.bson.UuidRepresentation
 import org.bukkit.plugin.java.JavaPlugin
-import org.litote.kmongo.*
+import org.litote.kmongo.KMongo
 import org.litote.kmongo.serialization.SerializationClassMappingTypeService
 
 class DatabaseManager(private val plugin: JavaPlugin) {
@@ -16,10 +18,15 @@ class DatabaseManager(private val plugin: JavaPlugin) {
         val connectionString =
             ConnectionString("mongodb+srv://${user}:${password}@${hostname}/${databaseName}?retryWrites=true&w=majority")
 
+        val settings: MongoClientSettings = MongoClientSettings.builder()
+            .applyConnectionString(connectionString)
+            .uuidRepresentation(UuidRepresentation.STANDARD)
+            .build()
+
         // Required property setter for applications loaded at runtime
         System.setProperty("org.litote.mongo.mapping.service", SerializationClassMappingTypeService::class.qualifiedName!!)
 
-        mongoClient = KMongo.createClient(connectionString)
+        mongoClient = KMongo.createClient(settings)
         db = mongoClient.getDatabase("tavern")
     }
 
