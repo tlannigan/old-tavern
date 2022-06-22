a8fdb7e69fd5959bd66f3e7726d05af7b3541068
tlannigan <tristanlannigan@gmail.com>
2022-06-22 16:01:42 -0600

    Implement character repository CRUD methods

--- src/main/kotlin/com/tlannigan/tavern/repositories/CharacterRepository.kt
+++ src/main/kotlin/com/tlannigan/tavern/repositories/CharacterRepository.kt
@@ -1,24 +1,31 @@
 package com.tlannigan.tavern.repositories
 
 import com.mongodb.client.MongoDatabase
+import com.mongodb.client.result.DeleteResult
+import com.mongodb.client.result.InsertOneResult
+import com.mongodb.client.result.UpdateResult
+import com.tlannigan.tavern.models.TCharacter
 import com.tlannigan.tavern.utils.DatabaseManager
+import org.litote.kmongo.*
 
-class CharacterRepository(private val db: MongoDatabase = DatabaseManager.db) {
+class CharacterRepository(db: MongoDatabase = DatabaseManager.db) {
 
-    fun create() {
+    private val characters = db.getCollection<TCharacter>("characters")
 
+    fun create(character: TCharacter): InsertOneResult {
+        return characters.insertOne(character)
     }
 
-    fun find() {
-
+    fun find(characterId: Id<TCharacter>): TCharacter? {
+        return characters.findOne(TCharacter::id eq characterId)
     }
 
-    fun update() {
-
+    fun update(character: TCharacter): UpdateResult {
+        return characters.updateOne(TCharacter::id eq character.id)
     }
 
-    fun delete() {
-
+    fun delete(character: TCharacter): DeleteResult {
+        return characters.deleteOne(TCharacter::id eq character.id)
     }
 
 }
