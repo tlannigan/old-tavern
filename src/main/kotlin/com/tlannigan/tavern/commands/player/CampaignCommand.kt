c4bafdac7934ecc34d5f312530072a08bcd097d5
tlannigan <tristanlannigan@gmail.com>
2022-06-23 12:29:49 -0600

    Add /campaign command handling

--- src/main/kotlin/com/tlannigan/tavern/commands/player/CampaignCommand.kt
+++ src/main/kotlin/com/tlannigan/tavern/commands/player/CampaignCommand.kt
@@ -1,5 +1,7 @@
 package com.tlannigan.tavern.commands.player
 
+import com.tlannigan.tavern.utils.getTPlayer
+import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabExecutor
@@ -8,10 +10,24 @@ import org.bukkit.entity.Player
 class CampaignCommand : TabExecutor {
 
     override fun onCommand(
-        sender: CommandSender, command: Command, label: String, args: Array<out String>
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<out String>
     ): Boolean {
         if (sender is Player) {
-            sender.sendMessage("You used the campaign command")
+            if (args.isNotEmpty()) {
+                val tPlayer = sender.getTPlayer()
+                if (tPlayer != null) {
+                    when (args[0].lowercase()) {
+                        "create" -> tPlayer.createCampaign(args, sender)
+                        "enter" -> tPlayer.enterCampaign(args, sender)
+                        "leave" -> tPlayer.leaveCampaign(sender)
+                    }
+                } else {
+                    Bukkit.getConsoleSender().sendMessage("Could not find this player")
+                }
+            }
         }
 
         return true
@@ -23,8 +39,9 @@ class CampaignCommand : TabExecutor {
         label: String,
         args: Array<out String>?
     ): MutableList<String>? {
-        // TODO("Not yet implemented")
-        return mutableListOf("arg1", "arg2", "arg3")
+//        val campaigns = CampaignRepository().findMany()
+//        return campaigns.map { it.name }.toMutableList()
+        return mutableListOf()
     }
 
 }
