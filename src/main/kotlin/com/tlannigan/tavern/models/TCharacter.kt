e798a410ff89caf4409ec707206641c6f56b26e7
tlannigan <tristanlannigan@gmail.com>
2022-06-23 12:28:54 -0600

    Refactor TCharacter from a top-level document into an embedded document

--- src/main/kotlin/com/tlannigan/tavern/models/TCharacter.kt
+++ src/main/kotlin/com/tlannigan/tavern/models/TCharacter.kt
@@ -1,23 +1,16 @@
 package com.tlannigan.tavern.models
 
 import kotlinx.serialization.Contextual
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import org.litote.kmongo.Id
-import org.litote.kmongo.newId
 import java.util.*
 
 @Serializable
 data class TCharacter(
 
-    @Contextual
-    @SerialName("_id")
-    val id: Id<TCharacter> = newId(),
-
     val uuid: @Contextual UUID,
 
     var name: String,
 
-    val state: PlayerState
+    var state: PlayerState
 
 )
