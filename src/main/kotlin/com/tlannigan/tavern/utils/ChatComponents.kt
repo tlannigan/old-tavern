8fc04d93ec740baaf1cd1338150cb94c354b520d
tlannigan <tristanlannigan@gmail.com>
2022-06-24 08:48:32 -0600

    Add ChatComponents util class

---
+++ src/main/kotlin/com/tlannigan/tavern/utils/ChatComponents.kt
@@ -0,0 +1,24 @@
+package com.tlannigan.tavern.utils
+
+import com.tlannigan.tavern.models.TCampaign
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.TextComponent
+import net.kyori.adventure.text.event.ClickEvent
+import net.kyori.adventure.text.format.NamedTextColor
+
+class ChatComponents {
+
+    companion object {
+
+        fun inviteCampaign(tCampaign: TCampaign): TextComponent {
+            return Component.text("You have been invited to ")
+                .append(
+                    Component.text("[JOIN]")
+                        .color(NamedTextColor.YELLOW)
+                        .clickEvent(ClickEvent.runCommand("/join ${tCampaign.id}")))
+                .append(Component.text(" ${tCampaign.name}"))
+        }
+
+    }
+
+}
