2bd69ff911ddf69bb5439b0a5dac531ca92aad1e
tlannigan <tristanlannigan@gmail.com>
2022-06-24 09:00:09 -0600

    Add kickPlayer method to TCampaign

--- src/main/kotlin/com/tlannigan/tavern/models/TCampaign.kt
+++ src/main/kotlin/com/tlannigan/tavern/models/TCampaign.kt
@@ -1,8 +1,12 @@
 package com.tlannigan.tavern.models
 
+import com.tlannigan.tavern.repositories.CampaignRepository
+import com.tlannigan.tavern.repositories.PlayerRepository
+import com.tlannigan.tavern.utils.getTPlayer
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.bukkit.Bukkit
 import org.litote.kmongo.Id
 import org.litote.kmongo.newId
 
@@ -29,6 +33,31 @@ data class TCampaign(
 
     val characters: MutableList<TCharacter> = mutableListOf()
 
-)
+) {
+
+    fun kickPlayer(characterName: String): Boolean {
+        val matchedCharacters = characters.count { it.name == characterName }
+
+        if (matchedCharacters == 1) {
+            val matchedCharacter = characters.find { it.name == characterName }
+
+            val tPlayer = Bukkit.getPlayer(matchedCharacter!!.uuid)?.getTPlayer()
+
+            if (tPlayer != null) {
+                tPlayer.leaveCampaign()
+                tPlayer.campaigns.removeAll { it == this.id }
+                PlayerRepository().update(tPlayer)
+
+                characters.removeAll { it.name == characterName }
+                CampaignRepository().update(this)
+
+                return true
+            }
+        }
+
+        return false
+    }
+
+}
 
 enum class SessionState { FREE, COMBAT, FROZEN }
