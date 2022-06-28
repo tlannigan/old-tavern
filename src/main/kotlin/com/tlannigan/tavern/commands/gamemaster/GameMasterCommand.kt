16f85a3466b6b42e10a23f56941c0a7d59b5cab4
tlannigan <tristanlannigan@gmail.com>
2022-06-27 20:28:28 -0600

    Implement campaign and game master commands

--- src/main/kotlin/com/tlannigan/tavern/commands/gamemaster/GameMasterCommand.kt
+++ src/main/kotlin/com/tlannigan/tavern/commands/gamemaster/GameMasterCommand.kt
@@ -1,6 +1,9 @@
 package com.tlannigan.tavern.commands.gamemaster
 
+import com.tlannigan.tavern.models.TPlayer
 import com.tlannigan.tavern.repositories.CampaignRepository
+import com.tlannigan.tavern.repositories.PlayerRepository
+import com.tlannigan.tavern.utils.ChatComponents
 import com.tlannigan.tavern.utils.getTPlayer
 import org.bukkit.Bukkit
 import org.bukkit.Bukkit.getConsoleSender
@@ -20,20 +23,21 @@ class GameMasterCommand : TabExecutor {
         if (sender is Player) {
             if (args.isNotEmpty()) {
                 val tPlayer = sender.getTPlayer()
-                if (tPlayer != null) {
+                val ownedCampaigns = tPlayer?.getCampaignsWhereGameMaster()
+                if (!ownedCampaigns.isNullOrEmpty()) {
                     when (args[0].lowercase()) {
-                        "start" -> tPlayer.startCampaign(args, sender)
-                        "end" -> tPlayer.endCampaign(sender)
-                        "invite" -> tPlayer.inviteCampaign(args, sender)
-                        "delete" -> tPlayer.deleteCampaign(args, sender)
+                        "start" -> startCampaign(sender, tPlayer, args)
+                        "end" -> endCampaign(sender, tPlayer)
+                        "invite" -> inviteCampaign(sender, tPlayer, args)
+                        "delete" -> deleteCampaign(sender, tPlayer, args)
                     }
-                } else {
-                    getConsoleSender().sendMessage("Could not find this player")
+
+                    return true
                 }
             }
         }
 
-        return true
+        return false
     }
 
     override fun onTabComplete(
@@ -51,7 +55,6 @@ class GameMasterCommand : TabExecutor {
                         val tPlayer = sender.getTPlayer()
                         if (tPlayer != null && args[1].isEmpty()) {
                             val campaigns = CampaignRepository().findMany(tPlayer.campaigns)
-                            Bukkit.getLogger().info(campaigns.toString())
                             return campaigns.map { it.name }.toMutableList()
                         }
                     }
@@ -60,6 +63,144 @@ class GameMasterCommand : TabExecutor {
         }
 
         return null
+    }
+
+    fun startCampaign(player: Player, gameMaster: TPlayer, args: Array<out String>) {
+        if (!gameMaster.hasActiveCampaign()) {
+            val campaignName = args[1]
+            val campaigns = CampaignRepository().findMany(gameMaster.campaigns)
+            val campaign = campaigns.find { it.name == campaignName }
+
+            if (campaign != null) {
+                if (campaign.gameMaster.uuid == player.uniqueId) {
+                    campaign.inSession = true
+                    val updatedCampaign = CampaignRepository().update(campaign)
+
+                    if (updatedCampaign.modifiedCount > 0) {
+                        player.sendMessage("Starting campaign...")
+                        player.performCommand("campaign enter $campaignName")
+                    }
+                } else {
+                    player.sendMessage("You are not the Game Master of this campaign.")
+                }
+            } else {
+                player.sendMessage("A campaign with this name does not exist.")
+            }
+        } else {
+            player.sendMessage("You must leave your current campaign session first.")
+        }
+    }
+
+    fun endCampaign(player: Player, gameMaster: TPlayer) {
+        val campaign = gameMaster.getActiveCampaign()
+        if (campaign != null) {
+            campaign.inSession = false
+            val updatedCampaign = CampaignRepository().update(campaign)
+
+            if (updatedCampaign.modifiedCount > 0) {
+                player.sendMessage("Ending campaign...")
+                player.performCommand("campaign leave")
+
+                val playersInSession = campaign.getPlayersInSession()
+                playersInSession.forEach {
+                    val bukkitPlayer = it.getBukkitPlayer()
+                    if (bukkitPlayer != null) {
+                        bukkitPlayer.performCommand("campaign leave")
+                    } else {
+                        getConsoleSender()
+                            .sendMessage("There was an error allowing ${it.id} to leave campaign session")
+                    }
+                }
+            }
+        } else {
+            player.sendMessage("You are not in a campaign session.")
+        }
+    }
+
+    fun inviteCampaign(player: Player, gameMaster: TPlayer, args: Array<out String>) {
+        if (gameMaster.hasActiveCampaign()) {
+            val playerName = args[1]
+            val invitee = Bukkit.getPlayerExact(playerName)
+            if (invitee != null) {
+                if (invitee.uniqueId != player.uniqueId) {
+                    val campaign = CampaignRepository().find(gameMaster.activeCampaign!!)
+                    if (campaign != null) {
+                        invitee.sendMessage(ChatComponents.inviteCampaign(campaign))
+                    }
+                } else {
+                    player.sendMessage("Silly Billy, you can't invite yourself!")
+                }
+            } else {
+                player.sendMessage("Could not find that player.")
+            }
+        } else {
+            player.sendMessage("Enter a campaign to invite others.")
+        }
+    }
+
+    fun kickPlayer(player: Player, gameMaster: TPlayer, args: Array<out String>): Boolean {
+        if (gameMaster.hasActiveCampaign()) {
+            val campaign = gameMaster.getActiveCampaign()
+            if (campaign != null) {
+                val characterName = args[1]
+                val matchedCharacters = campaign.characters.count { it.name == characterName }
+
+                if (matchedCharacters == 1) {
+                    val matchedCharacter = campaign.characters.find { it.name == characterName }
+
+                    val matchedPlayer = Bukkit.getPlayer(matchedCharacter!!.uuid)
+                    val tPlayer = player.getTPlayer()
+
+                    if (matchedPlayer != null && tPlayer != null) {
+                        val playerActiveCampaign = tPlayer.getActiveCampaign()
+                        if (playerActiveCampaign?.id == campaign.id) {
+                            player.performCommand("campaign leave")
+                        }
+
+                        tPlayer.campaigns.removeAll { it == campaign.id }
+                        PlayerRepository().update(tPlayer)
+
+                        campaign.characters.removeAll { it.name == characterName }
+                        CampaignRepository().update(campaign)
+
+                        return true
+                    }
+                }
+            }
+        }
+
+        return false
+    }
+
+    fun deleteCampaign(player: Player, gameMaster: TPlayer, args: Array<out String>) {
+        if (gameMaster.hasActiveCampaign()) {
+            if (args.size > 1 && args[1].lowercase() == "true") {
+                val campaign = gameMaster.getActiveCampaign()
+
+                if (campaign != null) {
+                    if (campaign.gameMaster.uuid == player.uniqueId) {
+                        endCampaign(player, gameMaster)
+
+                        campaign.characters.forEach {
+                            val campaignPlayer = Bukkit.getPlayer(it.uuid)
+                            player.performCommand("gm kick ${campaignPlayer?.name}")
+                        }
+
+                        gameMaster.campaigns.remove(campaign.id)
+                        PlayerRepository().update(gameMaster)
+
+                        player.sendMessage("Deleting campaign...")
+                        CampaignRepository().delete(campaign)
+                    }
+                } else {
+                    player.sendMessage("This campaign has already been deleted.")
+                }
+            } else {
+                player.sendMessage("Are you sure you want to delete this campaign? Use /gm delete true")
+            }
+        } else {
+            player.sendMessage("You must enter a campaign before using /gm delete")
+        }
     }
 
 }
--- src/main/kotlin/com/tlannigan/tavern/commands/player/CampaignCommand.kt
+++ src/main/kotlin/com/tlannigan/tavern/commands/player/CampaignCommand.kt
@@ -1,7 +1,11 @@
 package com.tlannigan.tavern.commands.player
 
+import com.tlannigan.tavern.models.PlayerState
+import com.tlannigan.tavern.models.TCampaign
+import com.tlannigan.tavern.models.TPlayer
 import com.tlannigan.tavern.repositories.CampaignRepository
-import com.tlannigan.tavern.utils.getTPlayer
+import com.tlannigan.tavern.repositories.PlayerRepository
+import com.tlannigan.tavern.utils.*
 import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
@@ -21,17 +25,23 @@ class CampaignCommand : TabExecutor {
                 val tPlayer = sender.getTPlayer()
                 if (tPlayer != null) {
                     when (args[0].lowercase()) {
-                        "create" -> tPlayer.createCampaign(args, sender)
-                        "enter" -> tPlayer.enterCampaign(args, sender)
-                        "leave" -> tPlayer.leaveCampaign(sender)
+                        "create" -> createCampaign(sender, tPlayer, args)
+                        "enter" -> enterCampaign(sender, tPlayer, args)
+                        "leave" -> leaveCampaign(sender, tPlayer)
                     }
+
+                    return true
                 } else {
-                    Bukkit.getConsoleSender().sendMessage("Could not find this player")
+                    Bukkit
+                        .getConsoleSender()
+                        .sendMessage(
+                            "An unexpected error occurred finding this player: ${sender.name} ${sender.uniqueId}"
+                        )
                 }
             }
         }
 
-        return true
+        return false
     }
 
     override fun onTabComplete(
@@ -56,7 +66,128 @@ class CampaignCommand : TabExecutor {
             }
         }
 
-        return mutableListOf()
+        return null
+    }
+
+    fun createCampaign(player: Player, tPlayer: TPlayer, args: Array<out String>) {
+        if (!tPlayer.hasActiveCampaign()) {
+            // Build initial campaign state
+            val gameMaster = player.buildCharacter()
+
+            val campaign = TCampaign(
+                name = args[1],
+                gameMaster = gameMaster,
+                spawn = player.location.toTLocation()
+            )
+
+            val savedCampaign = CampaignRepository().create(campaign)
+
+            if (savedCampaign.insertedId != null) {
+                // Update TPlayer with new campaign
+                tPlayer.campaigns.add(campaign.id)
+                val updatedPlayer = PlayerRepository().update(tPlayer)
+
+                if (updatedPlayer.modifiedCount > 0) {
+                    player.sendMessage("Campaign ${args[1]} created!")
+                } else {
+                    player.sendMessage("There was an issue adding the campaign to your account.")
+                }
+            } else {
+                player.sendMessage("There was an issue creating the campaign.")
+            }
+        } else {
+            player.sendMessage("You must leave your current campaign session first.")
+        }
+    }
+
+    fun enterCampaign(player: Player, tPlayer: TPlayer, args: Array<out String>) {
+        if (!tPlayer.hasActiveCampaign()) {
+            val campaignName = args[1]
+            val campaigns = CampaignRepository().findMany(tPlayer.campaigns)
+            val campaign = campaigns.find { it.name == campaignName }
+
+            if (campaign != null) {
+                if (campaign.gameMaster.uuid == player.uniqueId && !campaign.inSession) {
+                    player.performCommand("gm start $campaignName")
+                } else if (campaign.inSession) {
+                    // Store player's overworld state
+                    tPlayer.state = player.getPlayerState()
+                    tPlayer.activeCampaign = campaign.id
+                    PlayerRepository().update(tPlayer)
+
+                    // Set player character inSession to true
+                    val character = campaign.characters.find { it.uuid == player.uniqueId }
+                    if (character != null) {
+                        character.inSession = true
+                        CampaignRepository().update(campaign)
+                    }
+
+                    val campaignState: PlayerState?
+
+                    // Check if player is Game Master or player
+                    // and apply relevant state
+                    if (campaign.gameMaster.uuid == player.uniqueId) {
+                        campaignState = campaign.gameMaster.state
+                        player.applyState(campaignState)
+                    } else {
+                        val campaignCharacter = campaign.characters.find { it.uuid == player.uniqueId }
+                        if (campaignCharacter != null) {
+                            // Fetch player's state in campaign
+                            campaignState = campaignCharacter.state
+                            player.applyState(campaignState)
+                        } else {
+                            player.sendMessage("Could not find your character in this campaign.")
+                        }
+                    }
+                }
+            } else {
+                player.sendMessage("A campaign with this name does not exist.")
+            }
+        } else {
+            player.sendMessage("You must leave your current campaign session first.")
+        }
+    }
+
+    fun leaveCampaign(player: Player, tPlayer: TPlayer) {
+        val campaign = tPlayer.getActiveCampaign()
+
+        if (campaign != null) {
+            if (campaign.gameMaster.uuid == player.uniqueId && campaign.inSession) {
+                player.performCommand("gm end")
+            } else {
+                val campaignState = player.getPlayerState()
+
+                // Check if player is Game Master or player
+                // and save campaign state appropriately
+                if (campaign.gameMaster.uuid == player.uniqueId) {
+                    campaign.gameMaster.state = campaignState
+                    CampaignRepository().update(campaign)
+
+                    tPlayer.activeCampaign = null
+                    PlayerRepository().update(tPlayer)
+
+                    // Apply saved overworld state
+                    player.applyState(tPlayer.state)
+                } else {
+                    val campaignCharacter = campaign.characters.find { it.uuid == player.uniqueId }
+                    if (campaignCharacter != null) {
+                        // Save campaign state
+                        campaignCharacter.state = campaignState
+                        CampaignRepository().update(campaign)
+
+                        tPlayer.activeCampaign = null
+                        PlayerRepository().update(tPlayer)
+
+                        // Apply saved overworld state
+                        player.applyState(tPlayer.state)
+                    } else {
+                        player.sendMessage("There was an issue finding your character in this campaign.")
+                    }
+                }
+            }
+        } else {
+            player.sendMessage("You are not in a campaign session.")
+        }
     }
 
 }
