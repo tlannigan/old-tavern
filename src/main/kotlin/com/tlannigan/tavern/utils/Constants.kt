3b5536a71623f2ff48fd245aaf7196e073e1ad01
tlannigan <tristanlannigan@gmail.com>
2022-07-07 10:38:51 -0600

    Add constants for empty inventory Base64 strings

---
+++ src/main/kotlin/com/tlannigan/tavern/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.tlannigan.tavern.utils
+
+object Constants {
+
+    // Empty inventory Base64 strings
+    const val emptyContents = "rO0ABXcEAAAAKXBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBw"
+    const val emptyArmorContents = "rO0ABXcEAAAABHBwcHA="
+
+}
