52ebe869b8f5af4539a3e7b268f61b55c519fee4
tlannigan <tristanlannigan@gmail.com>
2022-07-08 08:49:58 -0600

    Make player leave campaign onPlayerQuit event

--- src/main/kotlin/com/tlannigan/tavern/listeners/PlayerListener.kt
+++ src/main/kotlin/com/tlannigan/tavern/listeners/PlayerListener.kt
@@ -1,10 +1,12 @@
 package com.tlannigan.tavern.listeners
 
 import com.tlannigan.tavern.repositories.PlayerRepository
+import com.tlannigan.tavern.utils.getTPlayer
 import com.tlannigan.tavern.utils.toTPlayer
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
 
 class PlayerListener : Listener {
 
@@ -13,7 +15,17 @@ class PlayerListener : Listener {
         val playerRepo = PlayerRepository()
         val playerExists = playerRepo.find(event.player.uniqueId)
         if (playerExists == null) {
-            PlayerRepository().create(event.player.toTPlayer())
+            playerRepo.create(event.player.toTPlayer())
+        }
+    }
+
+    @EventHandler
+    fun onPlayerQuit(event: PlayerQuitEvent) {
+        val tPlayer = event.player.getTPlayer()
+        val activeCampaign = tPlayer?.getActiveCampaign()
+
+        if (activeCampaign != null) {
+            event.player.performCommand("campaign leave")
         }
     }
 
